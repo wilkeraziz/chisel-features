4febdd92ebb8803df8e735e40058a825fdf538e9
vagrant <vagrant@precise64.(none)>
2014-09-18 13:12:58 +0000

    Negation scoring done

--- negation/main.py
+++ negation/main.py
@@ -5,6 +5,7 @@ from __future__ import division
 from client import client
 import logging
 import json
+from nltk.util import ngrams
 
 #dict(map(lambda x: zip(x.values()[0]['govIndex'],x.values()[0]['depIndex'])[0],a))
 
@@ -29,23 +30,19 @@ class Negation:
 		negDeps1 = filter(lambda x: x.keys()[0]=='neg',deps1)
 		negDeps2 = filter(lambda x: x.keys()[0]=='neg',deps2)
 		negCues1,negCues2 = self.extractCues(negDeps1,negDeps2)
-		# logging.info('negCues1')
-		# logging.info(negCues1)
-		# logging.info('negCues2')
-		# logging.info(negCues2)
 		negEvents1,negEvents2 = self.extractEvents(negDeps1,negDeps2)
-		# logging.info('negEvents1')
-		# logging.info(negEvents1)
-		# logging.info('negEvents2')
-		# logging.info(negEvents2)
 		scopes = self.extractScope([(negDeps1,govDepIndeces1,depLexical1),(negDeps2,govDepIndeces2,depLexical2)])
 
 
 		cueScore = self.scoreFmeasure(negCues1,negCues2)
-		# logging.info(cueScore)
 		eventScore = self.scoreFmeasure(negEvents1,negEvents2)
-		# logging.info(eventScore)
-		scopeScore = self.scoreScopeOverlap(None,None)
+		# scopes -> [ [[scope_hyp1]] ,[[scope_ref1],[scope_ref2]]]
+		scopeScore = self.scoreScopeOverlap(scopes[0],scopes[1])
+		totalScore = cueScore + eventScore + scopeScore
+
+		self.printScore(cueScore,eventScore,scopeScore,totalScore)
+
+		return totalScore
 
 	def extractCues(self,negDeps1,negDeps2):
 		negCues1 = map(lambda x: x['neg']['dep'],negDeps1) if negDeps1!=[] else []
@@ -71,17 +68,17 @@ class Negation:
 			logging.info(govDepIndeces)
 			#there can be multiple negations
 			negIndeces = map(lambda x: x['neg']['depIndex'],negDeps)
-			print negIndeces
-			if negIndeces == []: scopes.append(scope)
+			if negIndeces == []: scopes.append([[]])
 			else:
 				govIndeces = [x for x,y in govDepIndeces.items() if set(negIndeces).intersection(set(y))!=set([])]
-				scopes = self.getWordsInScope(negIndeces,govIndeces,govDepIndeces,depLexical)
+				scopes.append(self.getWordsInScope(negIndeces,govIndeces,govDepIndeces,depLexical))
+				logging.info(scopes)
 
 		return scopes
 
 	def getWordsInScope(self,negIndeces,govIndeces,govDepIndeces,depLexical):
 		
-		scopes = []
+		scopesInSentence = []
 
 		for govIndex in govIndeces:
 			indecesToRetrieve = [govIndex]
@@ -93,9 +90,9 @@ class Negation:
 					indecesToRetrieve.extend(govDepIndeces[currentIndex])
 			indecesToRetrieve = filter(lambda x: x not in negIndeces,indecesToRetrieve)
 			lexElements = map(lambda x: depLexical[str(x)],sorted(map(lambda x: int(x),indecesToRetrieve)))
-			scopes.append(lexElements)
+			scopesInSentence.append(lexElements)
 
-		return scopes
+		return scopesInSentence
 
 	def scoreFmeasure(self,hyp,ref):
 
@@ -112,9 +109,43 @@ class Negation:
 		fMeasure = (precision + recall) / 2 
 		return fMeasure
 
-	def scoreScopeOverlap(self,scope1,scope2):
+	def scoreScopeOverlap(self,scopeHyp,scopeRef):
 		
-		pass
+		totalScore = 0
+
+		for scope_h in scopeHyp:
+			bestScore = 0
+			for scope_r in scopeRef:
+
+				if scope_r==[] or scope_h==[]:
+					partialScore = 0
+					if partialScore > bestScore: bestScore = partialScore
+				else:
+					ngram_range=range(1,len(scope_h)+1)
+					logging.info("ngram_range")
+					logging.info(ngram_range)
+					score_weights=map(lambda x: round(x/reduce(lambda x,y:x+y,ngram_range),4),ngram_range)
+					logging.info(score_weights)
+				
+					partialScore=float()
+					for i in ngram_range:
+						hyp=ngrams(scope_h,i)
+						ref=ngrams(scope_r,i)
+						partialScore+=(len(set(hyp).intersection(set(ref)))*score_weights[i-1])
+					logging.info("partialScore")
+					logging.info(partialScore)
+					if partialScore > bestScore: bestScore = partialScore
+
+			totalScore+=bestScore
+			logging.info("totalScore")
+			logging.info(totalScore)
+			
+		return totalScore
+
+	def printScore(self,cueScore,eventScore,scopeScore,totalScore):
+
+		print "cueScore\teventScore\tscopeScore\ttotalScore\n\n"
+		print "%f\t%f\t%f\t%f" % (cueScore,eventScore,scopeScore,totalScore)
 
 def define_logging():
 
@@ -124,8 +155,8 @@ if __name__=="__main__":
 
 	define_logging()
 	n = Negation()
-	sent1 = u'She is not eating.'.encode('utf-8')
-	sent2 = u'She is not going to cinema because I said she is not going.'.encode('utf-8')
+	sent1 = u'She is not eating to the cinema because I said she is not going .'.encode('utf-8')
+	sent2 = u'She is going to cinema because I said she is going .'.encode('utf-8')
 	dep1 = n.get_dependencies(sent1,'en')
 	logging.info(dep1)
 	dep2 = n.get_dependencies(sent2,'en')
